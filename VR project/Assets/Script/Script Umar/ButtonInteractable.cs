437f36c5af71c1a5c6cf92e6187397ae16a1a08b
Umar Aziz <umaraziz0416@gmail.com>
2019-05-15 12:29:30 +0800

    Player animation

---
+++ VR project/Assets/Script/Script Umar/ButtonInteractable.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class ButtonInteractable : MonoBehaviour
+{
+    PlayerController controller;
+    public Transform interactionPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player Enter");
+            controller = other.GetComponent<PlayerController>();
+            controller.SetButtonTrigger(true);
+            controller.SetTriggerPosition(interactionPoint.position);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player Exit");
+            controller.SetButtonTrigger(false);
+            controller.SetTriggerPosition(Vector3.zero);
+        }
+    }
+}
