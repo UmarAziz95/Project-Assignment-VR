c70b1463d16efa616302ee22ce0807989672e253
Umar Aziz <umaraziz0416@gmail.com>
2019-05-19 15:48:26 +0800

    Fix Unity 2019 project import

---
+++ VR project/Assets/Script/Script Umar/TestScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using Random = System.Random;
+
+public class TestScript : NetworkBehaviour
+{
+    public Text text;
+
+    [SyncVar]
+    int num;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Random rand = new Random();
+        num = rand.Next(1, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = num.ToString();
+    }
+}
