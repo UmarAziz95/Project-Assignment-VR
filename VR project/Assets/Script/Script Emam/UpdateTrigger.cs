339a7b92f5e1c2457070b0706271f50d211ac910
Umar Aziz <umaraziz0416@gmail.com>
2019-05-19 14:39:33 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/UmarAziz95/Project-Assignment-VR""
    This reverts commit 93a2c991eba6140f4f571790c82f97733feb3e48.

---
+++ VR project/Assets/Script/Script Emam/UpdateTrigger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateTrigger : MonoBehaviour
+{
+    public int index;
+    public Material[]materialColor;
+    private Renderer rendererColor;
+    private bool isTriggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (index == TriggerProperties.currentTriggerIndex)
+            { 
+                rendererColor.sharedMaterial = materialColor[1];          
+                TriggerProperties.currentTriggerIndex++;
+                isTriggered = true;
+            }
+
+            else if(index != TriggerProperties.currentTriggerIndex && !isTriggered)
+            {
+                TriggerProperties.reset = true;
+                TriggerProperties.currentTriggerIndex = 1;
+            }
+        }
+    }
+
+    public void ResetMaterial()
+    {
+        rendererColor.sharedMaterial = materialColor[0];
+        isTriggered = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rendererColor = transform.parent.GetComponent<Renderer>();
+        rendererColor.sharedMaterial = materialColor[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
