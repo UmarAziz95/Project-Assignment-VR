b4256c82cf9b84baa785a1297a167d11f73d6f96
Umar Aziz <umaraziz0416@gmail.com>
2019-05-23 00:39:11 +0800

    Add timer

--- VR project/Assets/Script/Player Sript/stepPad.cs
+++ VR project/Assets/Script/Player Sript/stepPad.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR;
 
 public class stepPad : MonoBehaviour {
 
     // Use this for initialization
+    float timer;
 
     public bool enter = false;
     [SerializeField]
@@ -19,6 +21,11 @@ public class stepPad : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        if (!enter)
+        {
+            timer += Time.deltaTime;
+        }
+
         if (enter)
         {
             endgame();        }
@@ -32,6 +39,7 @@ public class stepPad : MonoBehaviour {
     public void endgame()
     {
         endgamepanel.SetActive(true);
+        endgamepanel.transform.Find("Timer").GetComponent<Text>().text = "Time Complete: " + timer.ToString("F");
         PausePanel.IsOn = endgamepanel.activeSelf;
         XRSettings.enabled = !XRSettings.isDeviceActive;
     }
